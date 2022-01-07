470658249f579b92ab339834a221dc13167df81f
MariaArnaudova <m.p.arnaudova@gmail.com>
2022-01-07 23:43:17 +0200

    Added solution.

---
+++ ArraysLab/03.RoundingNumbers/03.RoundingNumbers75.100.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.RoundingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read an array of real numbers (space separated), 
+            // round them in "away from 0" style and print the 
+            // output as in the examples:
+
+            string realNumbers = Console.ReadLine();
+            //double[] separatedNumbers = new double[realNumbers.Length]Split().
+            //                                      Select(double.Parse).ToArray();
+            string[] separatedNumbers = realNumbers.Split();
+            double[] numbers = new double[separatedNumbers.Length];
+            int[] rounNumbers = new int[numbers.Length];
+
+            for (int i = 0; i < rounNumbers.Length; i++)
+            {
+                numbers[i] = double.Parse(separatedNumbers[i]);
+                rounNumbers[i] = (int)Math.Round(numbers[i], MidpointRounding.AwayFromZero);
+
+            }
+
+            for (int j = 0; j < rounNumbers.Length; j++)
+            {
+                Console.WriteLine($"{numbers[j]} => {rounNumbers[j]}");
+                //Console.WriteLine("{0} => {1}", separatedNumbers[j], rounNumbers[j]);
+            }
+
+
+        }
+    }
+}
---
+++ ArraysLab/04.ReverseArrayOfStrings/04.ReverseArrayOfStrings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04.ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read an array of strings (space separated values), 
+            //  reverse it and print its elements:
+
+            string[] array = Console.ReadLine().Split();
+            string[] reverseArr = new string[array.Length];
+
+            //for (int i = 0; i < array.Length/2; i++)
+            //{
+            //    reverseArr[i] = array[array.Length -1-i];
+            //}
+
+            //for (int j = 0; j < array.Length; j++)
+            //{
+            //    Console.Write(reverseArr[j] + " ");
+            //}
+            for (int i = 0; i < array.Length; i++)
+            {
+                string left = array[i];
+                string right = array[array.Length - i - 1];
+                array[array.Length - i - 1] = left;
+                array[i] = right;
+            }
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+                Console.Write(String.Join(" ", array));
+            //}
+
+            
+        }
+    }
+}
