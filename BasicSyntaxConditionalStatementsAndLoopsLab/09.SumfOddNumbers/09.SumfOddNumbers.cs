5e26be336e28fb67d141cca536c41001d96cb87b
MariaArnaudova <m.p.arnaudova@gmail.com>
2022-02-17 12:08:15 +0200

    Added solution.

---
+++ BasicSyntaxConditionalStatementsAndLoopsLab/09.SumfOddNumbers/09.SumfOddNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _09.SumfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int oddCount = int.Parse(Console.ReadLine());
+            int sumNumbers = 0;
+            int oddnum = 1;
+            for (int i = 1; i <= oddCount; i++)
+            {   
+                Console.WriteLine(oddnum);
+                sumNumbers += oddnum;
+                oddnum += 2;
+                
+            }
+            Console.WriteLine($"Sum: {sumNumbers}");
+        }
+    }
+}
---
+++ BasicSyntaxConditionalStatementsAndLoopsLab/10.MultiplicationTable/10.MultiplicationTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _10.MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numTable = int.Parse(Console.ReadLine());
+            int result = 0;
+            for (int i = 1; i <=10; i++)
+            {
+                result = numTable * i;
+                Console.WriteLine($"{numTable} X {i} = {result}");
+            }
+        }
+    }
+}
