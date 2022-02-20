385a774155775e5cb2dadd545444174547e931c3
MariaArnaudova <m.p.arnaudova@gmail.com>
2022-02-20 21:32:30 +0200

    Added solution.

---
+++ BasicSyntaxConditionalStatementsAndLoopsExercise/01Ages/01Ages.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ages = int.Parse(Console.ReadLine());
+            string typePerson = "";
+            if (ages >= 0 && ages <= 2)
+            {
+                typePerson = "baby";
+            }
+            else if (ages >= 3 && ages <= 13)
+            {
+                typePerson = "child";
+            }
+            else if (ages >= 14 && ages <= 19)
+            {
+                typePerson = "teenager";
+            }
+            else if (ages >= 20 && ages <= 65)
+            {
+                typePerson = "adult";
+            }
+            else if (ages >= 66)
+            {
+                typePerson = "elder";
+            }
+            Console.WriteLine($"{typePerson}");
+        }
+    }
+}
